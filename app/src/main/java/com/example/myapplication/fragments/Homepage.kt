f81a59f01b163f215c85ea66d86be1b0071d39bc
LiagkaAikaterini <katerinaliaga1999@gmail.com>
2021-02-25 13:27:49 +0200

    database

--- app/src/main/java/com/example/myapplication/fragments/Homepage.kt
+++ app/src/main/java/com/example/myapplication/fragments/Homepage.kt
@@ -1,20 +1,25 @@
 package com.example.myapplication.fragments
 
+import android.content.ContentValues
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import androidx.constraintlayout.widget.Constraints.TAG
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.OrientationHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.MainActivity
 import com.example.myapplication.PlaylistAdapter
 import com.example.myapplication.PostsAdapter
 import com.example.myapplication.R
-import kotlinx.android.synthetic.main.fragment_homepage.*
-import java.util.ArrayList
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import java.util.*
+import kotlin.collections.ArrayList
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +32,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Homepage : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -47,13 +51,52 @@ class Homepage : Fragment() {
         val view = inflater.inflate(R.layout.fragment_homepage, container, false)
 
         val posts: ArrayList<String> = ArrayList()
+        val imageurl: ArrayList<String> = ArrayList()
+
+        var database = FirebaseDatabase.getInstance().reference
+        var getdata = object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //Log.d("mytag", "SEE HERE")
+                val recently: ArrayList<String> = ArrayList()
+                for (i in snapshot.child("user1").child("Playlists").child("1").child("SongArray").children){
+                    recently.add(i.value.toString())
+                    //Log.d("mytag", i.getValue().toString());
+                }
+                var j=0
+                for (i in recently) {
+                    var songName= snapshot.child(i).child("Name").value.toString()
+                    var songArtist = snapshot.child(i).child("Artist").value.toString()
+                    var caption : String = "$songName - $songArtist"
+                    var tr = caption.substring(0,4)
+                    posts.add(tr)
+                    var image = snapshot.child(i).child("ImageURL").value.toString()
+                    imageurl.add(image)
+                    //Log.d("mytag", posts[j])
+                    j++
+
+                }
+                val mRecyclerView: RecyclerView
+                mRecyclerView = view.findViewById(R.id.recyclerView)
+                mRecyclerView.layoutManager = LinearLayoutManager(activity as MainActivity, RecyclerView.HORIZONTAL, false)
+                mRecyclerView.adapter= PostsAdapter(posts, activity as MainActivity)
+            }
+        }
+        database.addValueEventListener(getdata)
+
+        //val ref = (activity as MainActivity).posts
+        //Log.d("mytag", ref[0])
+        /*val posts: ArrayList<String> = ArrayList()
         for (i in 1..100){
             posts.add("Song # $i")
-        }
-        val mRecyclerView: RecyclerView
+        }*/
+        //Log.d("mytag", "SEE HERE")
+        /*val mRecyclerView: RecyclerView
         mRecyclerView = view.findViewById(R.id.recyclerView)
         mRecyclerView.layoutManager = LinearLayoutManager(activity as MainActivity, RecyclerView.HORIZONTAL, false)
-        mRecyclerView.adapter= PostsAdapter(posts, activity as MainActivity)
+        mRecyclerView.adapter= PostsAdapter(ref, activity as MainActivity)*/
 
         val playlists: ArrayList<String> = ArrayList()
         for (i in 1..100){
@@ -86,6 +129,7 @@ class Homepage : Fragment() {
         return view
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -95,6 +139,7 @@ class Homepage : Fragment() {
         view.findViewById<ImageView>(R.id.home_image5).setOnClickListener {
             (activity as MainActivity).makeCurrentFragment(Playlist())
         }
+
         view.findViewById<ImageView>(R.id.home_image6).setOnClickListener {
             (activity as MainActivity).makeCurrentFragment(Playlist())
         }
